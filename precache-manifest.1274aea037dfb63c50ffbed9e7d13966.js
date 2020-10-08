c4e184808a11d4cb08cd5a51de5f3412226985d9
Verona <nebesnav@gmail.com>
2020-10-08 11:21:29 +0300

    Updates

---
+++ precache-manifest.1274aea037dfb63c50ffbed9e7d13966.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29ec08a240c28b3c36a820c5a74046e",
+    "url": "/blog_react.github.io/index.html"
+  },
+  {
+    "revision": "4f346dfd06daa57c52fa",
+    "url": "/blog_react.github.io/static/css/2.0168826d.chunk.css"
+  },
+  {
+    "revision": "3afbba4b2733d0d1d206",
+    "url": "/blog_react.github.io/static/css/main.6e483cd2.chunk.css"
+  },
+  {
+    "revision": "4f346dfd06daa57c52fa",
+    "url": "/blog_react.github.io/static/js/2.c65898ee.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/blog_react.github.io/static/js/2.c65898ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3afbba4b2733d0d1d206",
+    "url": "/blog_react.github.io/static/js/main.d2b762ea.chunk.js"
+  },
+  {
+    "revision": "5470fa74182aa70cb09a",
+    "url": "/blog_react.github.io/static/js/runtime-main.68e69e2e.js"
+  },
+  {
+    "revision": "3eba56a187f395275eccddd751703fe3",
+    "url": "/blog_react.github.io/static/media/about.3eba56a1.png"
+  },
+  {
+    "revision": "a10242f19e81a0a48c43bdebdf675a53",
+    "url": "/blog_react.github.io/static/media/back_1.a10242f1.png"
+  },
+  {
+    "revision": "5a72c4bfccabc80ac47b3e10aa827d4b",
+    "url": "/blog_react.github.io/static/media/back_2.5a72c4bf.png"
+  },
+  {
+    "revision": "0db7b1d8c08cb0ce8c205caf3b0039ae",
+    "url": "/blog_react.github.io/static/media/back_3.0db7b1d8.png"
+  },
+  {
+    "revision": "a10242f19e81a0a48c43bdebdf675a53",
+    "url": "/blog_react.github.io/static/media/hero-1.a10242f1.png"
+  },
+  {
+    "revision": "9e341e6ec6e7a7a5961c8a5f2ff80ea7",
+    "url": "/blog_react.github.io/static/media/logo.9e341e6e.png"
+  },
+  {
+    "revision": "2f6f17314e364770cb34d79a04a4da0c",
+    "url": "/blog_react.github.io/static/media/night_city.2f6f1731.jpg"
+  },
+  {
+    "revision": "2e480569197f5beb6c77cd8dfb90a1f3",
+    "url": "/blog_react.github.io/static/media/port1.2e480569.jpg"
+  },
+  {
+    "revision": "3a6ceb7d0e36692da3598ab6d6ea23a9",
+    "url": "/blog_react.github.io/static/media/port2.3a6ceb7d.jpg"
+  },
+  {
+    "revision": "de9e306c31bbd3c86f5b8ba488a8653e",
+    "url": "/blog_react.github.io/static/media/port3.de9e306c.jpg"
+  },
+  {
+    "revision": "bd2112e162f9255bfd106d2462d978a6",
+    "url": "/blog_react.github.io/static/media/port4.bd2112e1.jpg"
+  },
+  {
+    "revision": "04fb064777280a381ee6ad29e102d1ff",
+    "url": "/blog_react.github.io/static/media/pp1.04fb0647.jpg"
+  },
+  {
+    "revision": "89c6dbe683bc223ce3b44b95d3d56231",
+    "url": "/blog_react.github.io/static/media/pp2.89c6dbe6.jpg"
+  },
+  {
+    "revision": "a86b0a1f633618968c256cdfe8a05d8b",
+    "url": "/blog_react.github.io/static/media/pp4.a86b0a1f.jpg"
+  },
+  {
+    "revision": "9118f65b98524c421b98ab8fc7d593b8",
+    "url": "/blog_react.github.io/static/media/user-info.9118f65b.png"
+  }
+]);
