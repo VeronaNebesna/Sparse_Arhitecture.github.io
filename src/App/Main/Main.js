7f23d61cdf09a0648199fff66a7ec30d4af5cc5c
Verona <nebesnav@gmail.com>
2020-10-13 18:55:47 +0300

    fixed main.js

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -21,10 +21,7 @@ const Main = ({
             <Route path = "/" exact component={PortfolioDarkSection}/>
             <Route path = "/" exact component={ImgSection}/>
             <Route path="/" exact component={SliderFeddback}/>
-            <Route path ="/category"  exact render ={()=>(
-                 <CategoryPage
-               />
-            )}/> 
+            <Route path ="/category"  exact component={CategoryPage}/> 
             <Route path="/liked_post" component={LikedArticle}/>  
             <Route path="/read_more_about/:arrCategory" exact render={(match)=>(
                 <ReadMore
